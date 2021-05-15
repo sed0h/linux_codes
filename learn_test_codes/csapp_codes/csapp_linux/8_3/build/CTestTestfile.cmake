8d65a6b60cc50a095f7b4e8de36de16949379558
zgt <zhangguangtao@e-planet.cn>
2021-05-14 20:54:51 -0700

    add files, remove ./build/

--- learn_test_codes/csapp_codes/csapp_linux/8_3/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/zgt/csapp_codes/csapp_linux/8_3
-# Build directory: /home/zgt/csapp_codes/csapp_linux/8_3/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- learn_test_codes/leetcode_exercise/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/zgt/learn_test_codes/leetcode_exercise
-# Build directory: /home/zgt/learn_test_codes/leetcode_exercise/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
